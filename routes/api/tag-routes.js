7c52807c2740bcbf97e68c1ade03ce09453695c6
cloozo <mybenefitchoiceinc@gmail.com>
2022-01-22 20:15:03 -0500

    delete update method working for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,17 +42,62 @@ router.get("/:id", (req, res) => {
       res.status(404).json(error);
     });
 });
-
+//post route
+/* router.post("/", (req, res) => {
+  Tag.create(req.body)
+    .then((newProductTag) => {
+      res.json(newProductTag);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+}); */
 router.post("/", (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            tag_id: tag.id,
+            tag_id,
+          };
+        });
+        return Tag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
-router.put("/:id", (req, res) => {
-  // update a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+  const TagId = req.params.id;
+  const result = await Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: TagId,
+      },
+    }
+  );
+  res.json(result);
 });
 
-router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
+  const tagId = req.params.id;
+  const result4 = await Tag.destroy({
+    where: {
+      id: tagId,
+    },
+  });
+  res.json(result4);
 });
 
 module.exports = router;
